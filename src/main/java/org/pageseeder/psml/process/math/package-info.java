000fa3d6c7ea43058f3029a879cb53b3281c0e86
clauret <clauret@weborganic.com>
2025-12-16 14:03:40 +1100

    Chore: Simplify NullMarked annotation import in math package

--- src/main/java/org/pageseeder/psml/process/math/package-info.java
+++ src/main/java/org/pageseeder/psml/process/math/package-info.java
@@ -13,7 +13,5 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-@NullMarked
+@org.jspecify.annotations.NullMarked
 package org.pageseeder.psml.process.math;
-
-import org.jspecify.annotations.NullMarked;
